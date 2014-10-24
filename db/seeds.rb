b3d645ad751b35109cdc0019232ba4051f7459c7
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-24 19:43:18 -0400

    Updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,32 @@
-[
-  'University of Southern California',
-  'Boston University',
-  'New York University',
-  'University of Washington',
-  'Columbia University'
-].each do |name|
-  School.create(name: name)
-end
+School.create(
+  name: 'Boston University',
+  city: 'Boston',
+  state: 'MA'
+)
+
+School.create(
+  name: 'University of Southern California',
+  city: 'Los Angeles',
+  state: 'CA'
+)
+
+School.create(
+  name: 'Columbia University',
+  city: 'New York',
+  state: 'NY'
+)
+
+School.create(
+  name: 'New York University',
+  city: 'New York',
+  state: 'NY'
+)
+
+School.create(
+  name: 'University of Washington',
+  city: 'Seattle',
+  state: 'WA'
+)
 
 School.all.each do |school|
   ['Drink Beers!', 'Get In', 'Apply'].each do |title|
