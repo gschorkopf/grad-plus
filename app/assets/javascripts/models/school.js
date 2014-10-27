c68f5386bdbf08a26becd4f52fd1cf184a02f92a
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-27 16:23:24 -0400

    Adds more attributes to school js model

--- app/assets/javascripts/models/school.js
+++ app/assets/javascripts/models/school.js
@@ -1,5 +1,8 @@
 App.School = DS.Model.extend({
   name: DS.attr('string'),
+  dueDate: DS.attr('date'),
+  location: DS.attr('string'),
+  boardPassRate: DS.attr('number'),
   imageUrl: DS.attr('string'),
   tasks: DS.hasMany('task', { async: true })
 })
--- app/serializers/school_serializer.rb
+++ app/serializers/school_serializer.rb
@@ -1,5 +1,10 @@
 class SchoolSerializer < ActiveModel::Serializer
-  attributes :id, :name, :task_ids
+  attributes :id, :name, :task_ids, :location,
+    :board_pass_rate, :due_date, :image_url
+
+  def location
+    "#{object.city}, #{object.state}"
+  end
 
   def task_ids
     object.tasks.pluck(:id)
