f5aba6e840fb4f36d22a6096c41b07f2bdffff3b
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-27 16:28:43 -0400

    Turns location into a computed property

--- app/assets/javascripts/models/school.js
+++ app/assets/javascripts/models/school.js
@@ -1,8 +1,12 @@
 App.School = DS.Model.extend({
   name: DS.attr('string'),
   dueDate: DS.attr('date'),
-  location: DS.attr('string'),
+  city: DS.attr('string'),
+  state: DS.attr('string'),
   boardPassRate: DS.attr('number'),
   imageUrl: DS.attr('string'),
-  tasks: DS.hasMany('task', { async: true })
+  tasks: DS.hasMany('task', { async: true }),
+  location: function() {
+    return this.get('city') + ', ' + this.get('state');
+  }.property('city', 'state')
 })
--- app/serializers/school_serializer.rb
+++ app/serializers/school_serializer.rb
@@ -1,10 +1,6 @@
 class SchoolSerializer < ActiveModel::Serializer
-  attributes :id, :name, :task_ids, :location,
-    :board_pass_rate, :due_date, :image_url
-
-  def location
-    "#{object.city}, #{object.state}"
-  end
+  attributes :id, :name, :task_ids, :board_pass_rate, :due_date,
+  :image_url, :city, :state
 
   def task_ids
     object.tasks.pluck(:id)
