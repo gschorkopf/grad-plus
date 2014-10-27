f09811c913b2187f746d356f30114f8a5b0eaf12
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-27 16:23:40 -0400

    Adds some validations

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,5 @@
 class School < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, :city, :state, presence: true
   validates :name, uniqueness: true
 
   has_many :tasks, inverse_of: :school, dependent: :destroy
